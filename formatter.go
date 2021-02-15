3fcb920835124d37bc95ff660ccb98fc2ee8d3e1
Leonid Turchenkov <turchenkoff@gmail.com>
2021-02-15 22:16:09 +0700

    gin.H result

--- formatter.go
+++ formatter.go
@@ -1,7 +1,9 @@
 package rest_api_response_formatter
 
-func GetResponseData(data interface{}, errors *map[string][]string) *map[string]interface{} {
-    return &map[string]interface{}{
+import "github.com/gin-gonic/gin"
+
+func GetResponseData(data interface{}, errors *map[string][]string) *gin.H {
+    return &gin.H{
         "data":   data,
         "errors": *errors,
     }
